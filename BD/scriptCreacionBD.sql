7d849873d647a132bd1b5f3656b6e5b6f63bc041
Belén Gamero García <bgamero@cifpfbmoll.eu>
2022-05-25 20:55:07 +0200

    insert movimento

--- BD/scriptCreacionBD.sql
+++ BD/scriptCreacionBD.sql
@@ -97,36 +97,3 @@ CREATE TABLE turnos(
     FOREIGN KEY(idMovimiento) REFERENCES movimiento(id),
     FOREIGN KEY(idPokemon) REFERENCES pokemon(id)
 );
-
-INSERT INTO privilegio (nombre) VALUES ("básico");
-INSERT INTO avatarUsuario (rutaImagen, privilegio_ID) VALUES ("../Imagenes/EntrenadorBaseChico.png", 1);
-INSERT INTO avatarUsuario (rutaImagen, privilegio_ID) VALUES ("../Imagenes/EntrenadorBaseChica.png", 1);
-
--- Agua
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Floatzel", 280, 193, 103, 211, "../Imagenes/Floatzel.png");
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Empoleon", 278, 159, 162, 112, "../Imagenes/Empoleon.png");
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Gyarados", 300, 229, 146, 150, "../Imagenes/Gyarados.png");
-
--- Fuego
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Magmortar", 260, 175, 125, 153, "../Imagenes/Magmortar.png");
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Arcanine", 290 , 202, 148, 175, "../Imagenes/Arcanine.png");
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Entei", 340, 211, 157, 184, "../Imagenes/Entei.png");
-
--- Planta
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Serperior", 260, 139, 175, 207, "../Imagenes/Serperior.png");
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Sceptile", 250, 157, 121, 220, "../Imagenes/Sceptile.png");
-INSERT INTO pokemon (nombre, vida, ataque, defensa, velocidad, rutaImagen) VALUES ("Simisage", 260, 180, 117, 186, "../Imagenes/Simisage.png");
-
-delimiter $$
-DROP TRIGGER IF EXISTS revisarUsuarios $$
-CREATE TRIGGER revisarUsuarios before insert on usuario
-FOR EACH ROW
-BEGIN
-	DECLARE contador int;
-    set contador = (select COUNT(nombreUsuario) from usuario where lower(nombreUsuario)=lower(new.nombreUsuario));
-	IF(contador > 0) then
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Nombre de usuario repetido';
-	END IF;
-END; $$
-
-delimiter ;
