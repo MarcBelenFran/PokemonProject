b3f977bf18b9a14b10c1dafe2654ae92f11ef962
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2022-06-07 17:49:00 +0200

    DOCKER FINAL

--- Aplicacion/PokemonFBM/src/main/java/clasesApoyo/datosMysql.java
+++ Aplicacion/PokemonFBM/src/main/java/clasesApoyo/datosMysql.java
@@ -3,6 +3,6 @@ package clasesApoyo;
 public class datosMysql {
 	public static String driver = "com.mysql.cj.jdbc.Driver";
 	public static String driverUrl = "jdbc:mysql://db:3306/pokedexdb";
-	public static String user = "fran";
-	public static String password = "123456Fran";
+	public static String user = "prueba";
+	public static String password = "123456";
 }
